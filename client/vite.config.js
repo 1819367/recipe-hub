4f9aabcb61b53a4790e7d9b72d990a54434b013d
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-11-18 15:14:29 -0800

    Add proxy in vite.config.js for dev

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      //All API cals to /api will proxy to Flask on port 5000
+      '/api' : {
+          target: 'https://localhost:5000',
+          changeOrigin: true,
+      },
+    },
+  },
 })
