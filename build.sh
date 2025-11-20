6f834899cfb73e56e79bf25b918ea138f006e140
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-11-19 17:19:57 -0800

    add: build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+# Build the React frontend
 cd client
 npm install
 npm run build
+
+# Make sure Flask folders exist (this fixes the Render error)
+mkdir -p ../server/static ../server/templates
+
+# Copy the built React files to Flask
 cp -r dist/* ../server/static/
 cp dist/index.html ../server/templates/index.html
+
+# Install Python packages
 cd ../server
 pip install -r requirements.txt
